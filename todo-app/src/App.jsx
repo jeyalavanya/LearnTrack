daeabaa74d7cdda9c65f00a3a99b44fe922bed39
Jeya Lavanya J <jeyalavanya18@gmail.com>
2026-03-28 17:55:10 +0530

    Header components added

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -61,6 +61,7 @@ function App() {
 
   return (
     <div className="app">
+      <Header />
       <ToDoList
         todos={todos}
         addTodo={addTodo}
---
+++ todo-app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './../App.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>📝 My Todo List</h1>
+      <p>Add, Edit, Complete, Delete Tasks!</p>
+    </header>
+  );
+}
+
+export default Header;
